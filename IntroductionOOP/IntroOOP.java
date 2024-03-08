b59339430a5c59bc85c339e6055f080571fe07f0
Asif Ahammad <asifahammed359@gmail.com>
2024-03-08 22:12:19 +0600

    basic introduction in oop in java

--- IntroductionOOP/IntroOOP.java
+++ IntroductionOOP/IntroOOP.java
@@ -1,8 +1,36 @@
- package IntroductionOOP;
+package IntroductionOOP;
 
+/*
+ * Basic Intro of oop in java
+ */
 public class IntroOOP {
 
+    public class Television {
+        private int channel;
+        private int volume;
+
+        public void setChannel(int newChannel) {
+            this.channel = newChannel;
+        }
+
+        public void SetVolume(int newVolume) {
+            this.volume = newVolume;
+        }
+
+        public int getChannel() {
+            return channel;
+        }
+
+        public int getVolume() {
+            return volume;
+        }
+
+    }
+
     public static void main(String[] args) {
-        System.out.println("Asif");
+        IntroOOP intro = new IntroOOP();
+        Television t1 = intro.new Television();
+        t1.setChannel(10);
+        System.out.println("Channel: " + t1.getChannel());
     }
 }
