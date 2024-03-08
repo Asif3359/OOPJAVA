901a30e22697a509d7cdd339f52134a25ada53bb
Asif Ahammad <asifahammed359@gmail.com>
2024-03-08 23:44:14 +0600

    add a class name student

--- IntroductionOOP/IntroOOP.java
+++ IntroductionOOP/IntroOOP.java
@@ -25,48 +25,69 @@ class circle {
 }
 
 class Rectangle {
-    public double length ;
-    public double breadth ;
+    public double length;
+    public double breadth;
 
-    public double area()
-    {
-        return length*breadth;
+    public double area() {
+        return length * breadth;
     }
-    public double perimeter ()
-    {
-        return 2*(length+breadth);
+
+    public double perimeter() {
+        return 2 * (length + breadth);
     }
-    public boolean isSquare()
-    {
-        if(length == breadth)
-        {
-            return true ;
-        }
-        else
-        {
+
+    public boolean isSquare() {
+        if (length == breadth) {
+            return true;
+        } else {
             return false;
         }
     }
 }
-class Cylinder {
-    public double radius ;
-    public double height ;
 
-    public double lidArea()
-    {
-        return Math.PI*radius*radius;
+class Cylinder {
+    public double radius;
+    public double height;
+
+    public double lidArea() {
+        return Math.PI * radius * radius;
     }
-    public double totalSurfaceArea ()
-    {
-        return 2*lidArea()+circumference()*height;
+
+    public double totalSurfaceArea() {
+        return 2 * lidArea() + circumference() * height;
     }
-    public double volume()
-    {
-        return lidArea()*height;
+
+    public double volume() {
+        return lidArea() * height;
     }
-    public double circumference()
-    {
-        return  2*Math.PI*radius;
+
+    public double circumference() {
+        return 2 * Math.PI * radius;
+    }
+}
+class Student {
+    public int roll;
+    public String name;
+    public String course;
+    public int m1, m2, m3;
+
+    public int total() {
+        return m1 + m2 + m3;
+    }
+
+    public float average() {
+        return (float) total() / 3;
+    }
+
+    public char grade() {
+        if (average() >= 60)
+            return 'A';
+        else
+            return 'B';
+    }
+
+    public String toString() {
+        return "Roll No:" + roll + "\n" + "Name:" + name + "\n" + "Course:" + course + "\n" +"Total :"+total()+"\n";
     }
 }
 
@@ -119,20 +140,34 @@ public class IntroOOP {
         System.out.println("circumference Of Circle-2: " + c2.circumference());
 
         Rectangle rec1 = new Rectangle();
-        rec1.breadth = 4 ;
-        rec1.length = 4 ;
+        rec1.breadth = 4;
+        rec1.length = 4;
 
-        System.out.println("Area of Rectangle :"+rec1.area());
-        System.out.println("perimeter of Rectangle :"+rec1.perimeter());
-        System.out.println("isSquare of Rectangle :"+rec1.isSquare());
+        System.out.println("Area of Rectangle :" + rec1.area());
+        System.out.println("perimeter of Rectangle :" + rec1.perimeter());
+        System.out.println("isSquare of Rectangle :" + rec1.isSquare());
 
-        Cylinder  cyl1 = new Cylinder();
-        cyl1.height = 10 ;
-        cyl1.radius = 10 ;
+        Cylinder cyl1 = new Cylinder();
+        cyl1.height = 10;
+        cyl1.radius = 10;
 
-        System.out.println("lidArea of Cylinder :"+cyl1.lidArea());
-        System.out.println("totalSurfaceArea of Cylinder :"+cyl1.totalSurfaceArea());
-        System.out.println("volume of Cylinder :"+cyl1.volume());
+        System.out.println("lidArea of Cylinder :" + cyl1.lidArea());
+        System.out.println("totalSurfaceArea of Cylinder :" + cyl1.totalSurfaceArea());
+        System.out.println("volume of Cylinder :" + cyl1.volume());
+
+        Student s=new Student();
         
+        s.roll=1;
+        s.name="John";
+        s.course="CS";
+        s.m1=70;
+        s.m2=80;
+        s.m3=65;
+        
+        System.out.println("Total :"+s.total());
+        System.out.println("Average :"+s.average());
+        
+        System.out.println("Details:\n "+ s );
+
     }
 }
