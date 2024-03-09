4536e453504a53da5f730e5f8ab28d6dcc70552f
Asif Ahammad <asifahammed359@gmail.com>
2024-03-09 11:37:41 +0600

    Add bankInheritance and create some object for example

--- BankInheritance/BankInheritance.java
+++ BankInheritance/BankInheritance.java
@@ -11,6 +11,12 @@ class Account {
     private String dob;
     protected long balance;
 
+    // non Parametrised constructor
+    public Account() {
+        System.out.println("Non Parametrised  constructor ");
+    }
+
+    // Parametrised constructor
     public Account(String acc, String n, String add, String phone, String dob) {
         accNo = acc;
         name = n;
@@ -59,6 +65,7 @@ class Account {
  */
 class SavingsAccount extends Account {
     public SavingsAccount(String acc, String n, String add, String phone, String dob) {
+        // called Parametrised constructor
         super(acc, n, add, phone, dob); // use super for default constructor
     }
 
@@ -74,11 +81,14 @@ class SavingsAccount extends Account {
         }
     }
 }
+
 /**
  * LoanAccount
  */
 class LoanAccount extends Account {
+
     public LoanAccount(String acc, String n, String add, String phone, String dob) {
+        // called Parametrised constructor
         super(acc, n, add, phone, dob); // use super for default constructor
     }
 
@@ -101,7 +111,43 @@ class LoanAccount extends Account {
 
 public class BankInheritance {
     public static void main(String[] args) {
+        // Creating a SavingsAccount object
+        SavingsAccount savingsAccount = new SavingsAccount("SA001", "John Doe", "123 Main St", "123-456-7890",
+                "01-01-1990");
 
+        // Depositing some amount
+        savingsAccount.deposit(1000);
+
+        // Withdrawing some amount
+        savingsAccount.withdraw(500);
+
+        // Getting account details
+        System.out.println("Savings Account Details:");
+        System.out.println("Account Number: " + savingsAccount.getAccNo());
+        System.out.println("Name: " + savingsAccount.getName());
+        System.out.println("Address: " + savingsAccount.getAddress());
+        System.out.println("Phone: " + savingsAccount.getPhone());
+        System.out.println("Date of Birth: " + savingsAccount.getDOB());
+        System.out.println("Balance: " + savingsAccount.getBalance());
+
+        System.out.println();
+
+        // Creating a LoanAccount object
+        LoanAccount loanAccount = new LoanAccount("LA001", "Jane Smith", "456 Oak St", "987-654-3210", "02-02-1985");
+
+        // Paying an EMI
+        loanAccount.payEMI(200);
+
+        // Repaying the loan
+        loanAccount.repay(500);
+
+        // Getting account details
+        System.out.println("Loan Account Details:");
+        System.out.println("Account Number: " + loanAccount.getAccNo());
+        System.out.println("Name: " + loanAccount.getName());
+        System.out.println("Address: " + loanAccount.getAddress());
+        System.out.println("Phone: " + loanAccount.getPhone());
+        System.out.println("Date of Birth: " + loanAccount.getDOB());
+        System.out.println("Balance: " + loanAccount.getBalance());
     }
 }
-
